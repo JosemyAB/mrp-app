3d3ddef46b062a87c95c3d8ca422718f131179d2
JosemyAB <josemitd@gmail.com>
2024-01-27 23:58:04 +0100

    Implement actions

--- src/main/java/com/jmtebar/mrpapp/productionorders/ProductionOrder.java
+++ src/main/java/com/jmtebar/mrpapp/productionorders/ProductionOrder.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.util.StringUtils;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.Objects;
 
 @Getter
 public class ProductionOrder {
@@ -13,20 +14,56 @@ public class ProductionOrder {
     @Id
     private Long id;
     private String name;
-    private Date expectedCompletionDate;
+    private LocalDate expectedCompletionDate;
     private ProductionOrderState state;
+
 
     public enum ProductionOrderState {DRAFT, SUBMITTED, ACCEPTED, COMPLETED}
 
     public static ProductionOrder create(String name) throws Exception {
-
-        if (!StringUtils.hasLength(name)) {
-            throw new ValidationException("Order name is mandatory");
-        }
+        Objects.requireNonNull(name, "Order name is mandatory");
 
         ProductionOrder order = new ProductionOrder();
         order.name = name;
         order.state = ProductionOrderState.DRAFT;
         return order;
     }
+
+    public ProductionOrder renameTo(String newName) throws ValidationException {
+        Objects.requireNonNull(newName, "New name is mandatory");
+
+        if (state != ProductionOrderState.DRAFT) {
+            throw new IllegalStateException("Cannot rename production order in state " + state);
+        }
+
+        this.name = newName;
+
+        return this;
+    }
+
+    public ProductionOrder submit() {
+        if (state != ProductionOrderState.DRAFT) {
+            throw new IllegalStateException("Cannot submit order in state " + state);
+        }
+
+        this.state = ProductionOrderState.SUBMITTED;
+
+        return this;
+    }
+
+    public ProductionOrder accept(LocalDate expectedCompletionDate) {
+
+        if (state != ProductionOrderState.SUBMITTED) {
+            throw new IllegalStateException("Cannot accept production order in state " + state);
+        }
+
+        Objects.requireNonNull(expectedCompletionDate, "expectedCompletionDate is required to submit a production order");
+        if (expectedCompletionDate.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("Expected completion date must be in the future, but was " + expectedCompletionDate);
+        }
+        state = ProductionOrderState.ACCEPTED;
+        this.expectedCompletionDate = expectedCompletionDate;
+        return this;
+    }
+
 }
--- src/test/java/com/jmtebar/mrpapp/productionorders/ProductionOrderTest.java
+++ src/test/java/com/jmtebar/mrpapp/productionorders/ProductionOrderTest.java
@@ -1,7 +1,9 @@
 package com.jmtebar.mrpapp.productionorders;
 
-import com.jmtebar.mrpapp.ValidationException;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,9 +19,81 @@ class ProductionOrderTest {
         assertNull(order.getExpectedCompletionDate());
     }
 
-  @Test
-  void validatesNameIsPresent() {
-      ValidationException ex = assertThrows(ValidationException.class, () -> ProductionOrder.create(null));
-      assertEquals("Order name is mandatory", ex.getMessage());
-  }
+    @Test
+    void validatesNameIsPresentOnCreate() {
+        Exception ex = assertThrows(NullPointerException.class, () -> ProductionOrder.create(null));
+        assertEquals("Order name is mandatory", ex.getMessage());
+    }
+
+    @Test
+    void renamesProductionOrder() throws Exception {
+        String newName = "renamed-order";
+        ProductionOrder order = ProductionOrder.create("test-order");
+        assertEquals(newName, order.renameTo(newName).getName());
+    }
+
+    @Test
+    void validatesNameIsPresentOnRename() throws Exception {
+        ProductionOrder order = ProductionOrder.create("test-order");
+
+        Exception ex = assertThrows(NullPointerException.class, () -> order.renameTo(null));
+        assertEquals("New name is mandatory", ex.getMessage());
+    }
+
+    @Test
+    void validatesStatusIsDraftOnRename() throws Exception {
+        ProductionOrder order = ProductionOrder.create("test-order");
+        setPrivateField(order, "state", ProductionOrder.ProductionOrderState.ACCEPTED);
+
+        assertThrows(IllegalStateException.class, () -> order.renameTo("invalid-status"));
+    }
+
+    @Test
+    void submitsProductionOrder() throws Exception {
+        ProductionOrder order = ProductionOrder.create("test-order");
+        assertEquals(ProductionOrder.ProductionOrderState.SUBMITTED, order.submit().getState());
+    }
+
+    @Test
+    void validatesStatusIsDraftOnSubmit() throws Exception {
+        ProductionOrder order = ProductionOrder.create("test-order");
+        setPrivateField(order, "state", ProductionOrder.ProductionOrderState.ACCEPTED);
+
+        assertThrows(IllegalStateException.class, order::submit);
+    }
+
+
+    @Test
+    void acceptsProductionOrder() throws Exception {
+        ProductionOrder order = ProductionOrder.create("test-order");
+        setPrivateField(order, "state", ProductionOrder.ProductionOrderState.SUBMITTED);
+        assertEquals(ProductionOrder.ProductionOrderState.ACCEPTED, order.accept(LocalDate.now().plusDays(10)).getState());
+    }
+
+    @Test
+    void validatesStatusIsSubmittedOnAccept() throws Exception {
+        ProductionOrder order = ProductionOrder.create("test-order");
+        setPrivateField(order, "state", ProductionOrder.ProductionOrderState.SUBMITTED);
+
+        assertThrows(IllegalStateException.class, order::submit);
+    }
+
+    @Test
+    void validatesExpectedDateIsInFutureOnAccept() throws Exception {
+        ProductionOrder order = ProductionOrder.create("test-order");
+        setPrivateField(order, "state", ProductionOrder.ProductionOrderState.SUBMITTED);
+
+        assertThrows(IllegalArgumentException.class, () -> order.accept(LocalDate.now().minusDays(1)));
+    }
+
+
+    private void setPrivateField(Object object, String fieldName, Object value) {
+        try {
+            Field field = object.getClass().getDeclaredField(fieldName);
+            field.setAccessible(true);
+            field.set(object, value);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException("Failed to set private field " + fieldName, e);
+        }
+    }
 }
