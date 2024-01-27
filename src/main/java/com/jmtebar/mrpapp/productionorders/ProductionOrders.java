1c7c37fca86dcbe5ca9a7bf113d1033f52927d52
JosemyAB <josemitd@gmail.com>
2024-01-27 23:13:17 +0100

    Repository using RepositoryRestResource

---
+++ src/main/java/com/jmtebar/mrpapp/productionorders/ProductionOrders.java
@@ -0,0 +1,8 @@
+package com.jmtebar.mrpapp.productionorders;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductionOrders extends CrudRepository<ProductionOrder, Long> {
+}
