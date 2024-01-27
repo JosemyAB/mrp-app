9c45634bbdafdfec5fc66d0e1f71162827ddc70f
JosemyAB <josemitd@gmail.com>
2024-01-27 19:47:05 +0100

    Initial commit

---
+++ src/main/java/com/jmtebar/mrpapp/MrpAppApplication.java
@@ -0,0 +1,13 @@
+package com.jmtebar.mrpapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrpAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrpAppApplication.class, args);
+	}
+
+}
